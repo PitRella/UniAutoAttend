d6e7bb96b8c9045a62f6b08d341c5f2fc6c502e7
PitRella <serezha.sliva@gmail.com>
2025-09-06 23:47:38 +0300

    feat(group) - created group router

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import logging
 from fastapi import APIRouter, FastAPI
 
 from src.core.middleware import GlobalExceptionMiddleware
-from src.user.routers import user_router
+from src.user.routers import user_router, group_router
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +15,7 @@ app.add_middleware(GlobalExceptionMiddleware)
 
 main_api_router = APIRouter(prefix='/api/v1')
 main_api_router.include_router(user_router)
+main_api_router.include_router(group_router)
 
 app.include_router(main_api_router)
 
--- backend/src/user/routers/__init__.py
+++ backend/src/user/routers/__init__.py
@@ -1,1 +1,2 @@
 from .user import user_router
+from .group import group_router
