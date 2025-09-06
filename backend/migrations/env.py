e8bfe9db138afc7d32074773317343b76c93ca51
PitRella <serezha.sliva@gmail.com>
2025-09-06 19:02:38 +0300

    feat(migrations) - Init migration for db

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -27,8 +27,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from src.core.database import Base
-
-target_metadata = Base
+target_metadata = Base.metadata
 
 
 # other values from the config, defined by the needs of env.py,
