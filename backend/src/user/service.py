096df11328dfc0313e3c76610d1ca0e2d86e1183
PitRella <serezha.sliva@gmail.com>
2025-09-06 19:44:33 +0300

    feat(user) - Created service for user logic

---
+++ backend/src/user/service.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from src.core.dao import BaseDAO
+from src.core.service import BaseService
+from src.user.schemas import CreateUserRequestSchema
+from src.user.models import User
+
+
+class UserService(BaseService):
+    def __init__(
+            self,
+            db_session: AsyncSession,
+            user_dao: BaseDAO | None = None,
+    ):
+        super().__init__(db_session)
+        self._dao = user_dao or BaseDAO[
+            User,
+            CreateUserRequestSchema
+        ](db_session, model=User)
+
+    async def create_user(self, user_schema: CreateUserRequestSchema) -> User:
+        async with self._session.begin():
+            user: User = await self._dao.create(user_schema)
+        return user
