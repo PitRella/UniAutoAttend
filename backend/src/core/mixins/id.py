2b6663ee71cbc352c7d482324877900d7a37c5c5
PitRella <serezha.sliva@gmail.com>
2025-09-06 18:59:57 +0300

    feat(core) - Created mixins for id and timestamp

---
+++ backend/src/core/mixins/id.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from backend.src.core.database import Base
+
+
+class PrimaryKeyMixin(Base):
+    """Mixin class to add an auto-incrementing primary key `id` column.
+
+    This class is intended to be used as a base for SQLAlchemy models.
+    It is marked as abstract and does not create its own table.
+
+    Attributes:
+        id (Mapped[int]): Auto-incrementing primary key column.
+
+    """
+
+    __abstract__ = True
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+    )
