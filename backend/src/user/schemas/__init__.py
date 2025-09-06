63311630ed84a665106a1c48686d163d390cd42e
PitRella <serezha.sliva@gmail.com>
2025-09-06 23:33:15 +0300

    ref(user) - Added import to init

--- backend/src/user/schemas/__init__.py
+++ backend/src/user/schemas/__init__.py
@@ -1,3 +1,8 @@
 from .user import CreateUserRequestSchema, UpdateUserUnivInfoRequestSchema
+from .group import CreateGroupRequestSchema
 
-__all__ = ['CreateUserRequestSchema', 'UpdateUserUnivInfoRequestSchema']
+__all__ = [
+    'CreateUserRequestSchema',
+    'UpdateUserUnivInfoRequestSchema',
+    'CreateGroupRequestSchema'
+]
