14bd5e06a051b3a57e0451d35f0a150a218ac69c
PitRella <serezha.sliva@gmail.com>
2025-09-08 00:36:29 +0300

    feat(core) - changed pydantic field into dataclass

--- bot/src/core/schemas.py
+++ bot/src/core/schemas.py
@@ -1,17 +1,16 @@
 import re
 
-from pydantic import BaseModel, Field, EmailStr, SecretStr
-
+from dataclasses import dataclass
 from src.core.enum import UserState
 from src.core.locales import Language
 
 EMAIL_PATTERN = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
-
-class UserSchema(BaseModel):
+@dataclass(kw_only=True, slots=True)
+class UserSchema:
     """User data model."""
-    telegram_id: int = Field(..., ge=1)
+    telegram_id: int
     language: Language = Language.ENGLISH
     state: UserState = UserState.START
-    email: EmailStr | None = Field(None, pattern=EMAIL_PATTERN)
-    password: SecretStr | None = None
+    email: str | None
+    password: str | None
