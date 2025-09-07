0ebd8d354d6915b105459654fd39eea270b9dcd6
PitRella <serezha.sliva@gmail.com>
2025-09-07 23:58:04 +0300

    ref(*) - removed validation for password

--- bot/src/core/schemas.py
+++ bot/src/core/schemas.py
@@ -1,14 +1,9 @@
 import re
 
-from pydantic import BaseModel, Field, EmailStr, SecretStr, field_validator
+from pydantic import BaseModel, Field, EmailStr, SecretStr
 
 from src.core.enum import UserState
 from src.core.locales import Language
-from src.exceptions.models import BadPasswordSchemaException
-
-PASSWORD_PATTERN = re.compile(
-    r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$'
-)
 
 EMAIL_PATTERN = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
@@ -20,10 +15,3 @@ class UserSchema(BaseModel):
     state: UserState = UserState.START
     email: EmailStr | None = Field(None, pattern=EMAIL_PATTERN)
     password: SecretStr | None = None
-
-    @field_validator('password', mode='before')
-    def validate_password(cls, value: str) -> str:
-        """Validate the password using regex."""
-        if not PASSWORD_PATTERN.match(value):
-            raise BadPasswordSchemaException
-        return value
