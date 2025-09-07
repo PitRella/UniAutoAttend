68c8db1ae9d6bd27f7b484dd7ce719bc8c016afa
PitRella <serezha.sliva@gmail.com>
2025-09-07 13:27:17 +0300

    ref(services) - renamed validator into tg_validatior

--- bot/src/services/__init__.py
+++ bot/src/services/__init__.py
@@ -1,6 +1,6 @@
 from .user import UserService
 from .api import ApiService
-from .validator import TelegramValidatorService
+from .tg_validatior import TelegramValidatorService
 from src.core.settings import Settings
 
 settings = Settings.load()
