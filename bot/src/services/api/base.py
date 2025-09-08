3c00381a2b4fb2530cf1a608350a0709a4c178a6
PitRella <serezha.sliva@gmail.com>
2025-09-08 22:52:49 +0300

    feat(base) - created base api service

---
+++ bot/src/services/api/base.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+import aiohttp
+
+
+class BaseApiService(ABC):
+
+    def __init__(self, base_url: str, api_url: str):
+        self._base_url = base_url
+        self.api_url = api_url
+        self.timeout = aiohttp.ClientTimeout(total=10)
+        self.headers = {"Content-Type": "application/json"}
+
+    @abstractmethod
+    def send(self, *args, **kwargs):
+        pass
