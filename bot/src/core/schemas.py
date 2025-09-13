ba0fd6dc9039dcbbe7e2455fe0163c3b26f172d6
PitRella <serezha.sliva@gmail.com>
2025-09-13 17:00:35 +0300

    feat(core) - created request schema for creating a group for user

--- bot/src/core/schemas.py
+++ bot/src/core/schemas.py
@@ -4,9 +4,13 @@ from dataclasses import dataclass, asdict
 from src.core.enum import UserState
 from src.core.locales import Language
 
+@dataclass(kw_only=True, slots=True)
+class BaseSchemas:
+    def to_dict(self) -> dict:
+        return asdict(self)
 
 @dataclass(kw_only=True, slots=True)
-class UserSchema:
+class UserSchema(BaseSchemas):
     """User data model."""
     telegram_id: int
     username: str | None = None
@@ -16,8 +20,6 @@ class UserSchema:
     university_password: str | None = None
     university_group: str | None = None
 
-    def to_dict(self) -> dict:
-        return asdict(self)
 
 
 class CreateUserRequestSchema(BaseModel):
@@ -25,3 +27,8 @@ class CreateUserRequestSchema(BaseModel):
     username: str
     university_email: EmailStr
     university_password: str
+
+
+class SetGroupForUserRequestSchema(BaseModel):
+    telegram_id: int
+    name: str
