99c72a4dd0967af9b89ec8020746245f6622df50
PitRella <serezha.sliva@gmail.com>
2025-09-13 17:01:54 +0300

    feat(services) - created service for handle api group operations

--- bot/src/services/api/__init__.py
+++ bot/src/services/api/__init__.py
@@ -1,4 +1,5 @@
 from .user import UserApiService
+from .group import GroupApiService
 from src.core.settings import Settings
 
 settings = Settings.load()
@@ -6,4 +7,8 @@ settings = Settings.load()
 api_service = UserApiService(
     settings.api_settings.url,
     settings.api_settings.user_route,
+)
+group_service = GroupApiService(
+    settings.api_settings.url,
+    settings.api_settings.group_route,
 )
