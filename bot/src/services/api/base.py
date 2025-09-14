36cefcfab864cbd77312e8f27873b610e4b06a95
PitRella <serezha.sliva@gmail.com>
2025-09-14 12:58:51 +0300

    feat(api) - create api statuses enu, return status from send method not bool

--- bot/src/services/api/base.py
+++ bot/src/services/api/base.py
@@ -1,16 +1,23 @@
 from abc import ABC, abstractmethod
 
 import aiohttp
+from enum import StrEnum
+
+
+class ApiStatusesEnum(StrEnum):
+    ERROR = "error"
+    ALREADY_EXISTS = "already_exists"
+    SUCCESS = "success"
 
 
 class BaseApiService(ABC):
 
-    def __init__(self, base_url: str, api_url: str):
+    def __init__(self, base_url: str, api_url: str) -> None:
         self._base_url = base_url
         self.api_url = api_url
         self.timeout = aiohttp.ClientTimeout(total=10)
         self.headers = {"Content-Type": "application/json"}
 
     @abstractmethod
-    def send(self, *args, **kwargs):
+    def send(self, *args, **kwargs) -> ApiStatusesEnum:
         pass
